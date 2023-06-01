74fc5a044a1fcedb9c6a52c64128a0ebffa71306
Emre Guneyler <eguneys@gmail.com>
2023-06-02 01:29:34 +0300

    ehs_acc_test

---
+++ src/ehs_acc_test.ts
@@ -0,0 +1,22 @@
+import { EncodeCardsForNN, network } from './neural'
+
+import { Card, split_cards, make_deal } from 'lheadsup'
+import { ehs } from '../src/mcts'
+
+async function acc() {
+  let cards = split_cards(7, make_deal(2))
+  let hand = cards.slice(0, 2)
+  let board: Card[] = []
+
+  let expected = ehs(hand, board)
+  let computation = network.new_computation()
+  computation.AddInput(EncodeCardsForNN(hand, board))
+  await computation.ComputeAsync()
+  let got = computation.output[0][0]
+
+  console.log(hand.join(''), expected, got)
+}
+
+for (let i = 0; i < 10; i++) {
+  acc()
+}
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,4 @@
 import { ehs_train_main } from './ehs_train'
-import { EncodeCardsForNN, network } from './neural'
 
-import { Card, split_cards, make_deal } from 'lheadsup'
-import { ehs } from '../src/mcts'
 
-async function acc() {
-  let cards = split_cards(7, make_deal(2))
-  let hand = cards.slice(0, 2)
-  let board: Card[] = []
-
-  let expected = ehs(hand, board)
-  let computation = network.new_computation()
-  computation.AddInput(EncodeCardsForNN(hand, board))
-  await computation.ComputeAsync()
-  let got = computation.output[0][0]
-
-  console.log(hand.join(''), expected, got)
-}
-
-for (let i = 0; i < 10; i++) {
-  acc()
-}
+ehs_train_main()
