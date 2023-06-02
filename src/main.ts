927762115abaf9b7fbd5a34ea28a253942756d01
Emre Guneyler <eguneys@gmail.com>
2023-06-02 19:45:33 +0300

    cpu await

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { parallel_work } from './cluster'
 import { test_acc_main } from './ehs_acc_test'
 
 
-test_acc_main(100)
+//test_acc_main(100)
 
 //ehs_train_main()
-//parallel_work((cpus) => ehs_train_main(Math.ceil(100 / cpus)))
+parallel_work((cpus) => ehs_train_main(Math.ceil(100 / cpus)))
+
