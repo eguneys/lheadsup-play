c1863659a3b22ff780ae34e916b0f6a6efff5cb5
Emre Guneyler <eguneys@gmail.com>
2023-06-02 19:52:50 +0300

    args

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,11 @@ import { test_acc_main } from './ehs_acc_test'
 //test_acc_main(100)
 
 //ehs_train_main()
-parallel_work((cpus) => ehs_train_main(Math.ceil(100 / cpus)))
+
+
+let utilization = process.argv[2] === 'f' ? 100 : undefined
+
+console.log(utilization)
+
+parallel_work((cpus) => ehs_train_main(Math.ceil(100 / cpus)), utilization)
 
