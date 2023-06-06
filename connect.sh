2055b4ac8314f45808156d701604ecaa41574e8c
Emre Guneyler <eguneys@gmail.com>
2023-06-06 19:35:51 +0300

    connect

---
+++ connect.sh
@@ -0,0 +1,1 @@
+gcloud compute ssh --zone "us-central1-a" "instance-2" --project "lheadsup"
---
+++ setup.sh
@@ -0,0 +1,4 @@
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
+  sudo apt-get install -y nodejs
+
+curl -fsSL https://get.pnpm.io/install.sh | sh -
